112ffb212c6138ad04ab30f93680961564456619
Frank Bagehorn <fba@zurich.ibm.com>
2023-07-11 19:02:38 +0200

    Spring Initializr

---
+++ src/main/java/de/bagehorn/Haushaltsbuch/HaushaltsbuchApplication.java
@@ -0,0 +1,13 @@
+package de.bagehorn.Haushaltsbuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaushaltsbuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaushaltsbuchApplication.class, args);
+	}
+
+}
