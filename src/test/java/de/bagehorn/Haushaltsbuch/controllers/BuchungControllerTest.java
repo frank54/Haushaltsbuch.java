598e2214a9d2f5bf5c07826ce15e8eb0bf88c212
Frank Bagehorn <fba@zurich.ibm.com>
2023-09-22 13:50:25 +0200

    Unit Tests

---
+++ src/test/java/de/bagehorn/Haushaltsbuch/controllers/BuchungControllerTest.java
@@ -0,0 +1,85 @@
+package de.bagehorn.Haushaltsbuch.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.bagehorn.Haushaltsbuch.domain.Buchung;
+import de.bagehorn.Haushaltsbuch.domain.Kategorie;
+import de.bagehorn.Haushaltsbuch.exceptions.NotFoundException;
+import de.bagehorn.Haushaltsbuch.services.BuchungService;
+import de.bagehorn.Haushaltsbuch.services.KategorieService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BuchungController.class)
+public class BuchungControllerTest {
+    private static final Logger log = LoggerFactory.getLogger(BuchungControllerTest.class);
+    @Autowired
+    MockMvc mockmvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    BuchungService buchungService;
+    @MockBean
+    KategorieService kategorieService;
+
+    private Kategorie testKat;
+    private Buchung testBuchung;
+    private Buchung[] testBuchungen;
+
+    @BeforeEach
+    void setUp() throws JsonProcessingException {
+        String testKatStr = "{\"id\": 1, \"name\": \"Test\", \"position\": 1, \"typ\": \"Ausgaben\", \"beschreibung\": \"\"}";
+        testKat = objectMapper.readValue(testKatStr, Kategorie.class);
+        String testBuchungStr = "{\"id\": 1, \"betrag\": 666.65, \"datum\": \"2023-04-05\", \"beschreibung\": \"\", " +
+                "\"kategorie\": " + objectMapper.writeValueAsString(testKat) + "}";
+        testBuchung = objectMapper.readValue(testBuchungStr, Buchung.class);
+        testBuchungen = new Buchung[]{testBuchung};
+//        log.info(objectMapper.writeValueAsString(testBuchung));
+    }
+
+    @Test
+    void testGetBuchungen() throws Exception {
+        given(buchungService.findAll()).willReturn(Arrays.asList(testBuchungen));
+        mockmvc.perform(get("/api/v1/buchungen")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void testGetBuchungByKategorie() throws Exception {
+        given(buchungService.findByKategorie(testKat.getName())).willReturn(Arrays.asList(testBuchungen));
+        mockmvc.perform(get("/api/v1/buchungen/" + testKat.getName())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void testGetBuchungByKategorieKategorieNotFound() throws Exception {
+        given(buchungService.findByKategorie(testKat.getName())).willThrow(NotFoundException.class);
+        mockmvc.perform(get("/api/v1/buchungen/" + testKat.getName())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+}
