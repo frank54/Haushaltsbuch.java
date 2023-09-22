75a80e3aad5c700d3a81b4ba4574cbd4a7a8599a
frank54 <frank@bagehorn.de>
2023-09-22 15:27:21 +0200

    Lombok (#3)
    * JSON Serializer für die Buchung Klasse
    
    * Exception für nichtexistente Kategorien/Buchungen
    
    * Lombok Builder für Bootstrapping
    
    * Lombok Abhängigkeiten
    
    * Kategorien mit Lombok
    
    * Buchungen mit Lombok
    
    * Unit Tests
    
    * Warnungen beheben
    
    ---------
    
    Co-authored-by: Frank Bagehorn <fba@zurich.ibm.com>

--- src/main/java/de/bagehorn/Haushaltsbuch/services/KategorieServiceImpl.java
+++ src/main/java/de/bagehorn/Haushaltsbuch/services/KategorieServiceImpl.java
@@ -2,29 +2,25 @@ package de.bagehorn.Haushaltsbuch.services;
 
 import de.bagehorn.Haushaltsbuch.domain.Kategorie;
 import de.bagehorn.Haushaltsbuch.repositories.KategorieRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@AllArgsConstructor
 @Service
 public class KategorieServiceImpl implements KategorieService {
 
     private final KategorieRepository kategorieRepository;
     private final FilterService<Kategorie> filterService;
 
-    public KategorieServiceImpl(KategorieRepository kategorieRepository, FilterService<Kategorie> filterService) {
-        this.kategorieRepository = kategorieRepository;
-        this.filterService = filterService;
-    }
-
     @Override
     public Iterable<Kategorie> findAll() { return kategorieRepository.findAll(); }
 
     @Override
-    public Kategorie findByName(String name)
+    public Optional<Kategorie> findByName(String name)
     {
         Iterable<Kategorie> gefiltert = filterService.select(findAll(), k -> !k.getName().equals(name));
-        if (gefiltert.iterator().hasNext()) {
-            return gefiltert.iterator().next();
-        }
-        return null;
+        return Optional.of(gefiltert.iterator().next());
     }
 }
